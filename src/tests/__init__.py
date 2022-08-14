4492411cf9a3be455095b633b31043ade9d97e56
Nikolas <timofeevnik41@gmail.com>
2022-08-14 23:06:40 +0300

    :zap: removed api tests

--- src/tests/__init__.py
+++ src/tests/__init__.py
@@ -1,11 +1,6 @@
 import pytest
-from fastapi.testclient import TestClient
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
-
-# from .. import app
-
-# client = TestClient(app.app)
 
 
 @pytest.fixture
